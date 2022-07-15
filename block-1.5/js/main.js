895689df503717523d4ee73d4782e7e50f8106c1
distant007 <74708427+distant007@users.noreply.github.com>
2022-07-15 22:45:12 +0300

    normal

--- block-1.5/js/main.js
+++ block-1.5/js/main.js
@@ -1,48 +1,27 @@
-new Swiper('.slider', {
-  pagination:{
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  slidesPerView: 1.3,
-  spaceBetween: 15,
-  breakpoints: {
-    350: {
-      slidesPerView: 1.4
-    },
-    430: {
-      slidesPerView: 1.7
-    },
-    500: {
-      slidesPerView: 2
-    },
-    600: {
-      slidesPerView: 2.3
-    },
-    640: {
-      slidesPerView: 2.5
-    },
-    700:{
-      slidesPerView: 2.8
-    }
-
+window.onload = () => {
+  if (window.matchMedia('(max-width: 767px)').matches) {
+    const swiper = new Swiper('.swiper', {
+      direction: 'horizontal',
+      loop: true,
+      spaceBetween: 20,
+      slidesPerView: 'auto',
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+      },
+      init: true
+    })
   }
-});
-
-let buttons = document.querySelector('.brands__buttons');
-let showMore = document.querySelector('.brands__button-show');
-let hidden = document.querySelector('.brands__button-hidden');
-
-showMore.onclick = function () {
-  buttons.classList.toggle('height');
-  buttons.classList.toggle('height-more');
-  showMore.classList.toggle('hidden');
-  hidden.classList.toggle('show');
 }
-hidden.onclick = function () {
-  buttons.classList.toggle('height');
-  buttons.classList.toggle('height-more');
-  hidden.classList.toggle('show');
-  showMore.classList.toggle('hidden');
-}
+let swiper = document.querySelector('.swiper-wrapper');
+let open = document.querySelector('.slider__button');
+
+
+open.addEventListener('click', ()=>{
+  open.classList.toggle('hidden')
+  swiper.classList.toggle('opened')
+  open.textContent = !swiper.classList.contains('opened') ? 'Показать все' : 'Скрыть'
+})
+
 
 
